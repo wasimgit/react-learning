a72ec42440538ca3b565fcde6c704ecc455095a5
Wasim Khan <raju@ekfrazo.in>
2025-02-17 16:27:06 +0530

    Used the context API for basic understandings.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-
-
 import './App.css'
-import ToDoList from './Components/ToDoList'
+import User from './Components/User'
+import { ColorContext } from './ColorContext'
+import { useContext } from 'react';
 
 function App() {
+  const colors = useContext(ColorContext)
   return (
-      <ToDoList />
-  )
+        <ColorContext.Provider value={colors}>
+          <h1>This is a default app !</h1>
+          <User userName="my username" password="my password"/>
+        </ColorContext.Provider>
+  );
 }
 
 export default App
